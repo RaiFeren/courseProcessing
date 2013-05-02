95322e0ac5e30f6fe2651b2741ff19b7a103735d
Rai Feren <rai_feren@hmc.edu>
2013-05-02 16:42:20 -0700

    Added student structure and ability to parse students. Now need to parse for class data.

--- cpr.py
+++ cpr.py
@@ -7,6 +7,8 @@
 # Usage: `python cpr.py <data source file>`
 import re
 import os, sys, getopt
+import csv
+
 
 class Student(object):
     def __init__(self, mdata, gradYear):
@@ -18,26 +20,56 @@ class Student(object):
 
     def addClass(cid, yr, sem):
         self.classes.append( (cid, (yr, sem)) )
-    
-# students :: id -> Student
-students = {}
-# classes :: id -> (Year, Sem) -> [Student]
-classes = {}
+ 
+## Bottom lines will be removed in next commit!
+   
+# students :: sid -> Student
+#students = {}
+# classes :: cid -> (Year, Sem) -> [sid]
+#classes = {}
 
 
-def parse(rawText):
-    return []
+def parse(csvSrc):
+    # sdata :: sid -> Student
+    sdata = {}
+    # cdata :: cid -> (Year, Sem) -> [sid]
+    cdata = {}
+
+
+    spec = csvSrc.readline()    
+
+    curStudent = None
+
+    for line in csvSrc[1:]:
+        if not curStudent:
+            curStudent = Student(line[1],line[0])
+        curStudent.addClass(line[5], (line[3], line[4]))
+        
+        if line[7] != '':
+            # Save the student
+            if (len(curStudent.classes) != line[7]):
+                print "ERROR: Student's classes don't add up?!"
+            sdata[curStudent.id_] = curStudent
+            curStudent = None
+
+    if curStudent:
+        print "ERROR: Student left over?!"
+
+    students = sdata
+    return sdata, cdata
 
 def main(srcFile):
-    rawText = open(srcFile)
+    # Open the file.
+    rawText = open(srcFile, 'r')
     if not rawText:
         print "Problem with the source file!"
         return -1
-
-    # Open the file.
     # Parse it.
-    parse(srcFile)
+    textRead = csv.reader(rawText) 
+    studentData, courseData = parse(textRead)
+
     # Run tests on the parsed data
+
     # Render its results.
 
     return 0
