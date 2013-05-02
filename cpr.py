448003ff64507fa3633d7ffa5750b2088585e271
Rai Feren <rai_feren@hmc.edu>
2013-05-02 16:20:59 -0700

    Added basic classes and structures will be parsed into

--- cpr.py
+++ cpr.py
@@ -8,11 +8,32 @@
 import re
 import os, sys, getopt
 
+class Student(object):
+    def __init__(self, mdata, gradYear):
+        self.id_ = id(self)
+        self.college_ = mdata[0]
+        self.major_ = mdata[1:]
+        self.gradYear_ = gradYear
+        self.classes = []
+
+    def addClass(cid, yr, sem):
+        self.classes.append( (cid, (yr, sem)) )
+    
+# students :: id -> Student
+students = {}
+# classes :: id -> (Year, Sem) -> [Student]
+classes = {}
+
 
 def parse(rawText):
     return []
 
 def main(srcFile):
+    rawText = open(srcFile)
+    if not rawText:
+        print "Problem with the source file!"
+        return -1
+
     # Open the file.
     # Parse it.
     parse(srcFile)
