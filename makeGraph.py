a2b6adba3d17f58a6fba370ec5020e8f942a8dff
Beryl Egerter <begerter@hmc.edu>
2013-05-02 19:34:51 -0700

    ok, example graph function works properly now and is commented appropriately

--- makeGraph.py
+++ makeGraph.py
@@ -15,27 +15,36 @@ def exampleGraph(students, classes):
 	# here you would pull data from dictionaries but I'm just going to make stuff up
 	# instead
 
-	data = [(0,4,10,3), (0,3,5,7), (2,1,8,0)]
+	data = [(2008,0,3,6,3),(2009,0,4,10,2), (2010,1,3,7,4), (2011,1,4,5,4)]
 
 	theme.get_options()
-	# make graph
+
+	# draw graph
 	can = canvas.init("example.pdf")
-	chart_object.set_defaults(area.T, size = (150, 120), y_range = (0, 15),
-		x_coord = category_coord.T(data, 0))
-	chart_object.set_defaults(bar_plot.T, data = data)
+	chart_object.set_defaults(area.T, 
+		size = (150, 120), # size of graph
+		y_range = (0, 15), # bounds on y axis
+		x_coord = category_coord.T(data, 0)) #where the x coord info comes from
 
+	chart_object.set_defaults(bar_plot.T, data = data) #set it as a bar plot, give data
+
+	# reset in between drawing things?
 	bar_plot.fill_styles.reset()
-	plot1=bar_plot.T(label="freshmen", cluster=(0,3))
-	plot2=bar_plot.T(label="sophomores", hcol=2, cluster=(1,3))
-	plot3=bar_plot.T(label="juniors", hcol=3, cluster=(2,3))
-	#plot4=bar_plot.T(label="seniors", hcol=4, cluster=(3,4))
 
+	# to make legend
+	# for clusters, first number is order of occurence, second 
+	# number is how many columns there are
+	plot1=bar_plot.T(label="freshmen", cluster=(0,4))
+	plot2=bar_plot.T(label="sophomores", hcol=2, cluster=(1,4))
+	plot3=bar_plot.T(label="juniors", hcol=3, cluster=(2,4))
+	plot4=bar_plot.T(label="seniors", hcol=4, cluster=(3,4))
+
+	# draw legend
 	ar = area.T(loc=(250,0),
             x_axis=axis.X(label="X label", format="/a-30{}%d"),
             y_axis=axis.Y(label="Y label", tic_interval=2))
-	ar.add_plot(plot1, plot2, plot3)
+	ar.add_plot(plot1, plot2, plot3, plot4)
 	ar.draw()
-	for x in (ar.x_pos(10) - 20, ar.x_pos(20)- 10, ar.x_pos(30) - 10):
-		zap.zap_horizontally(can, line_style.default, fill_style.white,
-    		x, ar.y_pos(65), x+16, ar.y_pos(65) + 4, 4, 4)
+
+	# clean up after ourselves
 	can.close()
